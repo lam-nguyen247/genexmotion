e69795fb7dd94e11ea8ccf3242b0058b9060cbd9
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-03-08 15:37:54 +0700

    Update index.blade.php

--- resources/views/home/service/index.blade.php
+++ resources/views/home/service/index.blade.php
@@ -1,7 +1,7 @@
 @extends('home.layouts.app')
 
-@section('title', 'SocMedia Animation  - SocMedia Motion')
-@section('description', 'SocMedia Animation ĐĂNG KÝ Đăng ký Làm Video Motion Graphic Chuyên Nghiệp 00 00 00 00 Ngày Giờ Phút Giây Đăng ký ngay để nhận ưu đãi 300.000VNĐ THU HÚT VÀ ĐÁNH TRÚNG TÂM LÝKHÁCH HÀNG NHỜ VIDEO ANIMATION BỪNG SÁNG THƯƠNG HIỆU – BÙNG NỔ DOANH SỐ Nhờ chuyển động đẹp mắt')
+@section('title', 'SocMedia - '. $postServices->name)
+@section('description', $postServices->content)
 @section('css')
 @endsection
 

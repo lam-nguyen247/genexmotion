3c8411b15cb5a655e618a2146917ca9ab3975e60
Nguyễn Thành Lâm <lam.nt@neo-lab.vn>
2022-04-15 23:28:33 +0700

    Update index.blade.php

--- resources/views/home/service/index.blade.php
+++ resources/views/home/service/index.blade.php
@@ -10,6 +10,10 @@
 @endsection
 
 @section('js')
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
     <script type="text/javascript" src="/js/home/servicesHome.js"></script>
     <script>
        function clickContact(){

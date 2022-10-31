71a1c9b64378356841cc2471f64ed7f009fc75d9
Nguyễn Thành Lâm <lam.nt@neo-lab.vn>
2022-10-31 21:52:28 +0700

    Update HomeController.php

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -16,6 +16,6 @@ class HomeController extends Controller
     public function services($parentSlug, $subSlug)
     {
         $postList = [];
-        return view('home.post.index', compact('postList'));
+        return view('home.service.index', compact('postList'));
     }
 }

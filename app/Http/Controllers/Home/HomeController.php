e4f04f145d1587a759d67a7cbce5a8de188e2520
Nguyễn Thành Lâm <lam.nt@neo-lab.vn>
2022-10-31 21:26:32 +0700

    Update HomeController.php

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -15,6 +15,7 @@ class HomeController extends Controller
     }
     public function services($parentSlug, $subSlug)
     {
+        dd($subSlug);
         $postServices = Category::with('serviceList')->where('slug', $subSlug)->first();
         dd($postServices);
         return view('home.service.index', compact('postServices'));

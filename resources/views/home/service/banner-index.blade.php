befb45d92567bbadf1917834e38ab5dbe66ad432
Nguyễn Thành Lâm <lam.nt@neo-lab.vn>
2022-10-31 21:35:45 +0700

    Update banner-index.blade.php

--- resources/views/home/service/banner-index.blade.php
+++ resources/views/home/service/banner-index.blade.php
@@ -1,2 +1,23 @@
- {{-- SECTION POST SERVICES --}}
- @include('home.service.post-service')
+<div id="content" role="main" class="content-area">
+  <div class="ladi-wraper">
+    {{-- SECTION ONE --}}
+
+    {{-- END SECTION ONE --}}
+
+    {{-- SECTION TWO --}}
+    <div id="SECTION308" class="ladi-section">
+      <div class="ladi-container">
+        {!!$postServices->content!!}
+      </div>
+    </div>
+    {{-- END SECTION TWO --}}
+
+
+     {{-- SECTION POST SERVICES --}}
+     @include('home.service.post-service')
+
+
+
+  </div>
+</div>
+</div>

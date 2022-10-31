7a925e60ab1cd6d1d8d43cd2473e6789680ed6a4
Nguyễn Thành Lâm <lam.nt@neo-lab.vn>
2022-10-31 21:04:28 +0700

    Update index.blade.php

--- resources/views/home/service/index.blade.php
+++ resources/views/home/service/index.blade.php
@@ -4,13 +4,20 @@
 @section('description', $postServices->name)
 
 @section('content')
-{{-- @include('home.service.banner-index') --}}
+<div class="row align-center page-wrapper">
+    <div class="large-20 col">
+        <div class="row large-columns-3 medium-columns- small-columns-1 has-shadow row-box-shadow-1">
+
+        </div>
+    </div>
+</div>
+    {{-- @include('home.service.banner-index') --}}
 @endsection
 
 @section('js')
 
-    {{-- <script type="text/javascript" src="/js/home/servicesHome.js"></script> --}}
-    {{-- <script>
+    <script type="text/javascript" src="/js/home/servicesHome.js"></script>
+    <script>
        function clickContact(){
         var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
         var validate = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g;
@@ -31,5 +38,5 @@
             alert("Vui lòng nhập họ tên và số điện thoại");
         }
        }
-    </script> --}}
+    </script>
 @endsection

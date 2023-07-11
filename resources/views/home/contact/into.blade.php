f05b10430ec97a8aeec2aedecbc966edb100e618
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:01:42 +0700

    Update into.blade.php

--- resources/views/home/contact/into.blade.php
+++ resources/views/home/contact/into.blade.php
@@ -1,7 +1,5 @@
 
 <section class="section dark" id="section_474277286">
-    <div class="bg section-bg fill bg-fill  bg-loaded">
-    </div>
     <div class="section-content relative">
         <div class="row row-collapse row-full-width align-middle" id="row-2000811393">
             <div id="col-2122624723" class="col medium-12 small-12 large-6">
@@ -10,23 +8,11 @@
                         <div id="col-710186185" class="col medium-10 small-10 large-8">
                             <div class="col-inner">
                                 <div class="is-divider divider clearfix"
-                                     style="max-width:100px;height:4px;background-color:rgb(253, 111, 94);"></div>
+                                     style="max-width:100px;height:4px;background-color:#E8B6411F;"></div>
                                 <div id="text-3261000050" class="text">
 
                                     <h1 class="uppercase" data-cms="{{'vi'}}-into-index-5">sự khác biệt của
                                         SocMedia</h1>
-
-                                    <style>
-                                        #text-3261000050 {
-                                            font-size: 1.5rem;
-                                        }
-
-                                        @media (min-width: 550px) {
-                                            #text-3261000050 {
-                                                font-size: 1.2rem;
-                                            }
-                                        }
-                                    </style>
                                 </div>
                                 <div id="text-1186976077" class="text">
                                     <p data-cms="{{'vi'}}-into-index-6">Tự tin là đơn vị Motion Graphic Agency với mức
@@ -44,80 +30,15 @@
                                         giảm giá thành sản xuất xuống còn 50% so với
                                         các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
                                         lượng video
-                                        thành phẩm.<br/>
-
-                                        <style>
-                                            #text-1186976077 {
-                                                font-size: 1rem;
-                                                line-height: 1.55;
-                                            }
-                                        </style>
-                                </div>
-                                <div id="gap-1057699983" class="gap-element clearfix"
-                                     style="display:block; height:auto;">
-                                    <style>
-                                        #gap-1057699983 {
-                                            padding-top: 10px;
-                                        }
-                                    </style>
+                                        thành phẩm.
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                        <style>
-                            #row-2057224049 > .col > .col-inner {
-                                padding: 20px 0px 20px 0px;
-                            }
 
-                            @media (min-width: 550px) {
-                                #row-2057224049 > .col > .col-inner {
-                                    padding: 60px 0px 60px 0px;
-                                }
-                            }
-
-                            @media (min-width: 850px) {
-                                #row-2057224049 > .col > .col-inner {
-                                    padding: 60px 0px 40px 0px;
-                                }
-                            }
-                        </style>
                     </div>
-                </div>
-            </div>
-            <div id="col-1381582237" class="col medium-12 small-12 large-5 medium-col-first">
-                <div class="col-inner">
-                    <div class="img has-hover x md-x lg-x y md-y lg-y" id="image_1802229025">
-                        <div class="img-inner dark">
-                            <img width="658" data-cms="img-inner-dark-2" height="638"
-                                 @src="/images/2021/11/one-peace-flow-png-2.png"
-                                 class="attachment-large size-large" alt="" loading="lazy"
-                                 sizes="(max-width: 658px) 100vw, 658px"/>
-                        </div>
-                        <style>
-                            #image_1802229025 {
-                                width: 100%;
-                            }
-                        </style>
-                    </div>
-
                 </div>
             </div>
         </div>
     </div>
-    <style>
-        #section_474277286 {
-            padding-top: 0px;
-            padding-bottom: 0px;
-            background-color: rgb(36, 38, 42);
-        }
-
-        #section_474277286 .ux-shape-divider--top svg {
-            height: 150px;
-            --divider-top-width: 100%;
-        }
-
-        #section_474277286 .ux-shape-divider--bottom svg {
-            height: 150px;
-            --divider-width: 100%;
-        }
-    </style>
 </section>

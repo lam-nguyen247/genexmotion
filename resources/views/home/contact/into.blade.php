e6bb4876591ff177158df008fb0b91ab87498476
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:16:49 +0700

    Update into.blade.php

--- resources/views/home/contact/into.blade.php
+++ resources/views/home/contact/into.blade.php
@@ -6,5 +6,12 @@
             <p data-cms="{{'vi'}}-into-index-6"> SOC MEDIA là công ty cung cấp dịch vụ đa lĩnh vực như: truyền thông, quảng cáo, báo chí, mạng xã hội. Chuyên sản xuất Video truyền thông chất lượng  theo yêu cầu của khách hàng  với : </p>
             <strong>Chi phí hợp lý nhất - Chất lượng nhất - Hiệu quả nhất</strong>
         </div>
+        <div style="position: absolute; right: 10px; top: 30px;">
+            <svg width="234" height="234" viewBox="0 0 234 234" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M42 119.598C40 118.443 40 115.557 42 114.402L89.25 87.1221C91.25 85.9674 93.75 87.4108 93.75 89.7202L93.75 144.28C93.75 146.589 91.25 148.033 89.25 146.878L42 119.598Z" fill="#E8B641" fill-opacity="0.31"/>
+                <path d="M111 119.598C109 118.443 109 115.557 111 114.402L140.25 97.5144C142.25 96.3597 144.75 97.8031 144.75 100.113L144.75 133.887C144.75 136.197 142.25 137.64 140.25 136.486L111 119.598Z" fill="#E8B641" fill-opacity="0.31"/>
+                <path d="M162 119.598C160 118.443 160 115.557 162 114.402L191.25 97.5144C193.25 96.3597 195.75 97.8031 195.75 100.113L195.75 133.887C195.75 136.197 193.25 137.64 191.25 136.486L162 119.598Z" fill="#E8B641" fill-opacity="0.31"/>
+            </svg>
+        </div>
     </div>
 </section>

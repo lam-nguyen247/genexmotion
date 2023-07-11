1c920a757a2065011e5e9d0972add8be29a14b3d
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:05:10 +0700

    Update into.blade.php

--- resources/views/home/contact/into.blade.php
+++ resources/views/home/contact/into.blade.php
@@ -1,42 +1,24 @@
 
-<section class="section dark" id="section_474277286">
-    <div class="section-content relative">
-        <div class="row row-collapse row-full-width align-middle" id="row-2000811393">
-            <div id="col-2122624723" class="col medium-12 small-12 large-12">
-                <div class="col-inner">
-                    <div class="row align-center" id="row-2057224049">
-                        <div id="col-710186185" class="col medium-10 small-10 large-8">
-                            <div class="col-inner">
-                                <div id="text-3261000050" class="text">
-
-                                    <h1 class="uppercase" data-cms="{{'vi'}}-into-index-5">sự khác biệt của
-                                        SocMedia</h1>
-                                </div>
-                                <div id="text-1186976077" class="text">
-                                    <p data-cms="{{'vi'}}-into-index-6">Tự tin là đơn vị Motion Graphic Agency với mức
-                                        chi phí hợp lý nhất đồng thời gia tăng chất
-                                        lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
-                                        xuất ý
-                                        tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video.</p>
-                                    <p data-cms="{{'vi'}}-into-index-7">“Learn Creative Marketing”<br/>
-                                        Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
-                                        những ý tưởng đồng
-                                        thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
-                                        thông nhanh
-                                        chóng, chính xác, đánh trúng tâm lý khách hàng.</p>
-                                    <p data-cms="{{'vi'}}-into-index-8">Đồng thời, SocMedia áp dụng “One-piece Flow” để
-                                        giảm giá thành sản xuất xuống còn 50% so với
-                                        các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
-                                        lượng video
-                                        thành phẩm.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+<section class="section">
+    <div class="section-content relative" style="text-align: center">
+        <h2>Lời giới thiệu Socmedia</h2>
+        <p data-cms="{{'vi'}}-into-index-6">Tự tin là đơn vị Motion Graphic Agency với mức
+            chi phí hợp lý nhất đồng thời gia tăng chất
+            lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
+            xuất ý
+            tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video.</p>
+        <p data-cms="{{'vi'}}-into-index-7">“Learn Creative Marketing”<br/>
+            Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
+            những ý tưởng đồng
+            thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
+            thông nhanh
+            chóng, chính xác, đánh trúng tâm lý khách hàng.</p>
+        <p data-cms="{{'vi'}}-into-index-8">Đồng thời, SocMedia áp dụng “One-piece Flow” để
+            giảm giá thành sản xuất xuống còn 50% so với
+            các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
+            lượng video
+            thành phẩm.
+        </p>
+        <strong>Chi phí hợp lý nhất - Chất lượng nhất - Hiệu quả nhất</strong>
     </div>
 </section>

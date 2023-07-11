0b1865b1b80dcaebfa33ed0f14948d387a089465
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:09:45 +0700

    Update into.blade.php

--- resources/views/home/contact/into.blade.php
+++ resources/views/home/contact/into.blade.php
@@ -3,22 +3,8 @@
     <div class="section-content relative" style="text-align: center;">
         <div style="width: 50%; margin: 0px auto">
             <h2>Lời giới thiệu Socmedia</h2>
-            <p data-cms="{{'vi'}}-into-index-6">Tự tin là đơn vị Motion Graphic Agency với mức
-                chi phí hợp lý nhất đồng thời gia tăng chất
-                lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
-                xuất ý
-                tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video.</p>
-            <p data-cms="{{'vi'}}-into-index-7">“Learn Creative Marketing”<br/>
-                Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
-                những ý tưởng đồng
-                thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
-                thông nhanh
-                chóng, chính xác, đánh trúng tâm lý khách hàng.</p>
-            <p data-cms="{{'vi'}}-into-index-8">Đồng thời, SocMedia áp dụng “One-piece Flow” để
-                giảm giá thành sản xuất xuống còn 50% so với
-                các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
-                lượng video
-                thành phẩm.
+            <p data-cms="{{'vi'}}-into-index-6">SOC MEDIA là công ty cung cấp dịch vụ đa lĩnh vực như: truyền thông, quảng cáo, báo chí, mạng xã hội.
+                Chuyên sản xuất Video truyền thông chất lượng  theo yêu cầu của khách hàng  với :
             </p>
             <strong>Chi phí hợp lý nhất - Chất lượng nhất - Hiệu quả nhất</strong>
         </div>

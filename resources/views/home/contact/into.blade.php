670205a7b5dc147ea77b7ef24267f638177c74f1
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:11:28 +0700

    Update into.blade.php

--- resources/views/home/contact/into.blade.php
+++ resources/views/home/contact/into.blade.php
@@ -2,10 +2,8 @@
 <section class="section">
     <div class="section-content relative" style="text-align: center;">
         <div style="width: 50%; margin: 0px auto">
-            <h2>Lời giới thiệu Socmedia</h2>
-            <p data-cms="{{'vi'}}-into-index-6">SOC MEDIA là công ty cung cấp dịch vụ đa lĩnh vực như: truyền thông, quảng cáo, báo chí, mạng xã hội.
-                Chuyên sản xuất Video truyền thông chất lượng  theo yêu cầu của khách hàng  với :
-            </p>
+            <h2 style="text-transform: uppercase">Lời giới thiệu Socmedia</h2>
+            <p data-cms="{{'vi'}}-into-index-6"> SOC MEDIA là công ty cung cấp dịch vụ đa lĩnh vực như: truyền thông, quảng cáo, báo chí, mạng xã hội. Chuyên sản xuất Video truyền thông chất lượng  theo yêu cầu của khách hàng  với : </p>
             <strong>Chi phí hợp lý nhất - Chất lượng nhất - Hiệu quả nhất</strong>
         </div>
     </div>

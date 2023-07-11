496febab5f0daeb6de4a0e561ccd9544f59295f3
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:07:24 +0700

    Update into.blade.php

--- resources/views/home/contact/into.blade.php
+++ resources/views/home/contact/into.blade.php
@@ -1,24 +1,26 @@
 
-<section class="section" style="text-align: center;">
-    <div class="section-content relative" style="text-align: center; max-width: 50%">
-        <h2>Lời giới thiệu Socmedia</h2>
-        <p data-cms="{{'vi'}}-into-index-6">Tự tin là đơn vị Motion Graphic Agency với mức
-            chi phí hợp lý nhất đồng thời gia tăng chất
-            lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
-            xuất ý
-            tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video.</p>
-        <p data-cms="{{'vi'}}-into-index-7">“Learn Creative Marketing”<br/>
-            Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
-            những ý tưởng đồng
-            thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
-            thông nhanh
-            chóng, chính xác, đánh trúng tâm lý khách hàng.</p>
-        <p data-cms="{{'vi'}}-into-index-8">Đồng thời, SocMedia áp dụng “One-piece Flow” để
-            giảm giá thành sản xuất xuống còn 50% so với
-            các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
-            lượng video
-            thành phẩm.
-        </p>
-        <strong>Chi phí hợp lý nhất - Chất lượng nhất - Hiệu quả nhất</strong>
+<section class="section">
+    <div class="section-content relative" style="text-align: center;">
+        <div style="width: 50%">
+            <h2>Lời giới thiệu Socmedia</h2>
+            <p data-cms="{{'vi'}}-into-index-6">Tự tin là đơn vị Motion Graphic Agency với mức
+                chi phí hợp lý nhất đồng thời gia tăng chất
+                lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
+                xuất ý
+                tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video.</p>
+            <p data-cms="{{'vi'}}-into-index-7">“Learn Creative Marketing”<br/>
+                Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
+                những ý tưởng đồng
+                thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
+                thông nhanh
+                chóng, chính xác, đánh trúng tâm lý khách hàng.</p>
+            <p data-cms="{{'vi'}}-into-index-8">Đồng thời, SocMedia áp dụng “One-piece Flow” để
+                giảm giá thành sản xuất xuống còn 50% so với
+                các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
+                lượng video
+                thành phẩm.
+            </p>
+            <strong>Chi phí hợp lý nhất - Chất lượng nhất - Hiệu quả nhất</strong>
+        </div>
     </div>
 </section>

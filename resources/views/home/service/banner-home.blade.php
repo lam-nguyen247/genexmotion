8ea9408268e3f0b9535e70baf7f1edc1bf53e952
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-11 09:48:13 +0700

    Update banner-home.blade.php

--- resources/views/home/service/banner-home.blade.php
+++ resources/views/home/service/banner-home.blade.php
@@ -1,65 +1,59 @@
 <section style="padding: 60px 0px; margin-top: 10px;">
-    <div class="row row-collapse row-full-width">
-        <div class="col small-12 large-12">
-            <div class="col-inner">
-                <div class="text-center uppercase">
-                    <h3><strong><span style="color: #282828;" data-cms="{{app()->getLocale()}}-service-home-1">NHỮNG DỊCH VỤ MÀ SOCMEDIA ĐANG CÓ </span></strong>
-                    </h3>
-                </div>
-                <div class="row row-collapse row-full-width align-center">
-                    <div class="col col-md-3  col-sm-12 service-item">
-                        <div class="service-image">
-                            <img src="/images/san-xuat-video.png" alt="sản xuất video"/>
-                        </div>
-                        <h3 data-cms="{{app()->getLocale()}}-service-home-video-title">Sản Xuất Video</h3>
-                        <p data-cms="{{app()->getLocale()}}-service-home-video-content">
-                            Tự tin là đơn vị Motion Graphic Agency với mức
-                            chi phí hợp lý nhất đồng thời gia tăng chất
-                            lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
-                            xuất ý
-                            tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video
-                        </p>
-                        <a href="" class="service-link">
-                            <i class="icon-angle-right"></i>
-                        </a>
-                    </div>
-
-                    <div class="col col-md-3 col-sm-12 service-item">
-                        <div class="service-image">
-                            <img src="/images/dich-vu-marketing.png" alt="Dịch vụ Marketing"/>
-                        </div>
-                        <h3 data-cms="{{app()->getLocale()}}-service-home-marketing-title">Dịch Vụ Marketing</h3>
-                        <p data-cms="{{app()->getLocale()}}-service-home-marketing-content">
-                            “Learn Creative Marketing”<br/>
-                            Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
-                            những ý tưởng đồng
-                            thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
-                            thông nhanh
-                            chóng, chính xác, đánh trúng tâm lý khách hàng.
-                        </p>
-                        <a href="" class="service-link">
-                            <i class="icon-angle-right"></i>
-                        </a>
-                    </div>
-
-                    <div class="col col-md-3  col-sm-12 service-item">
-                        <div class="service-image">
-                            <img src="/images/an-pham-quang-cao.png" alt="Ấn phẩm Quảng Cáo"/>
-                        </div>
-                        <h3 data-cms="{{app()->getLocale()}}-service-home-ads-title">Ấn Phẩm Quảng Cáo</h3>
-                        <p data-cms="{{app()->getLocale()}}-service-home-ads-content">
-                            Đồng thời, SocMedia áp dụng “One-piece Flow” để
-                            giảm giá thành sản xuất xuống còn 50% so với
-                            các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
-                            lượng video
-                            thành phẩm.
-                        </p>
-                        <a href="" class="service-link">
-                            <i class="icon-angle-right"></i>
-                        </a>
-                    </div>
-                </div>
+    <div class="text-center uppercase">
+        <h3><strong><span style="color: #282828;" data-cms="{{app()->getLocale()}}-service-home-1">NHỮNG DỊCH VỤ MÀ SOCMEDIA ĐANG CÓ </span></strong>
+        </h3>
+    </div>
+    <div class="row row-collapse row-full-width align-center">
+        <div class="col large-3 small-12 service-item">
+            <div class="service-image">
+                <img src="/images/san-xuat-video.png" alt="sản xuất video"/>
             </div>
+            <h3 data-cms="{{app()->getLocale()}}-service-home-video-title">Sản Xuất Video</h3>
+            <p data-cms="{{app()->getLocale()}}-service-home-video-content">
+                Tự tin là đơn vị Motion Graphic Agency với mức
+                chi phí hợp lý nhất đồng thời gia tăng chất
+                lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
+                xuất ý
+                tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video
+            </p>
+            <a href="" class="service-link">
+                <i class="icon-angle-right"></i>
+            </a>
+        </div>
+
+        <div class="col large-3 small-12 service-item">
+            <div class="service-image">
+                <img src="/images/dich-vu-marketing.png" alt="Dịch vụ Marketing"/>
+            </div>
+            <h3 data-cms="{{app()->getLocale()}}-service-home-marketing-title">Dịch Vụ Marketing</h3>
+            <p data-cms="{{app()->getLocale()}}-service-home-marketing-content">
+                “Learn Creative Marketing”<br/>
+                Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
+                những ý tưởng đồng
+                thời rút ngắn thời gian lên kịch bản chủ đạo; tạo ra những thông điệp truyền
+                thông nhanh
+                chóng, chính xác, đánh trúng tâm lý khách hàng.
+            </p>
+            <a href="" class="service-link">
+                <i class="icon-angle-right"></i>
+            </a>
+        </div>
+
+        <div class="col large-3 small-12 service-item">
+            <div class="service-image">
+                <img src="/images/an-pham-quang-cao.png" alt="Ấn phẩm Quảng Cáo"/>
+            </div>
+            <h3 data-cms="{{app()->getLocale()}}-service-home-ads-title">Ấn Phẩm Quảng Cáo</h3>
+            <p data-cms="{{app()->getLocale()}}-service-home-ads-content">
+                Đồng thời, SocMedia áp dụng “One-piece Flow” để
+                giảm giá thành sản xuất xuống còn 50% so với
+                các đơn vị Motion Graphic Agency khác trên thị trường mà vẫn giữ nguyên chất
+                lượng video
+                thành phẩm.
+            </p>
+            <a href="" class="service-link">
+                <i class="icon-angle-right"></i>
+            </a>
         </div>
     </div>
 

e2c1abbc17302356e4963a2e441a80af3fde0816
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-13 13:37:45 +0700

    Update consultation.blade.php

--- resources/views/home/includes/consultation.blade.php
+++ resources/views/home/includes/consultation.blade.php
@@ -1,25 +1,21 @@
-<div class="grid-action">
-    <div class="container">
-        <div class="row">
-            <div class="medium-1 large-2"></div>
-            <div class="large-12 medium-10 large-8">
-                <div class="box-action row">
-                    <div class="box-action--content large-12 medium-6">
-                        <h3 class="title" data-cms="{{'vi'}}-includes-consultation-1">Bạn cần sự tư vấn</h3>
-                        <div class="sapo" data-cms="{{'vi'}}-includes-consultation-2">Hãy liên hệ với chúng tôi!</div>
-                    </div>
-                    <div class="box-action--form large-12 medium-6 d-flex align-items-center">
-                        <form class="form-group js-form">
-                            <input type="text" class="form-control" name="phone" placeholder="Số điện thoại của bạn"
-                                   required>
-                            <button type="submit" class="btn btn-form">
-                                <span>@lang('Send')</span>
-                            </button>
-                        </form>
-                    </div>
+<div class="container">
+    <div class="row">
+        <div class="large-12 medium-10 large-8">
+            <div class="box-action row">
+                <div class="box-action--content large-12 medium-6">
+                    <h3 class="title" data-cms="{{'vi'}}-includes-consultation-1">Bạn cần sự tư vấn</h3>
+                    <div class="sapo" data-cms="{{'vi'}}-includes-consultation-2">Hãy liên hệ với chúng tôi!</div>
+                </div>
+                <div class="box-action--form large-12 medium-6 d-flex align-items-center">
+                    <form class="form-group js-form">
+                        <input type="text" class="form-control" name="phone" placeholder="Số điện thoại của bạn"
+                               required>
+                        <button type="submit" class="btn btn-form">
+                            <span>@lang('Send')</span>
+                        </button>
+                    </form>
                 </div>
             </div>
-            <div class="medium-1 large-2"></div>
         </div>
     </div>
 </div>

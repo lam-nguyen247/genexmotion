d17ae71b759966c68e775bd38edb5668b954962f
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-13 14:31:02 +0700

    Update chanel-form.blade.php

--- resources/views/home/includes/chanel-form.blade.php
+++ resources/views/home/includes/chanel-form.blade.php
@@ -3,51 +3,47 @@
         @csrf
         <div class="container">
             <div class="row" style="margin: 0px  0px 20px 0px  !important; padding: 0px 15px;">
-                <div class="large-6 small-12" style="order: 1 !important">
-                    <div class="row">
-                        <div class="large-12 small-12">
-                            <label style="margin-right:10px;">Giá: </label>
-                        </div>
-                        <div class="large-6 small-12">
-                            <input
-                                class="chanel-filter"
-                                style=" margin-bottom: 10px; margin-left: 5px;"
-                                placeholder="từ"
-                                value="{{isset($_GET['price_f'])?$_GET['price_f']: ''}}" type="number"
-                                id="price_f" name="price_f">
-                        </div>
-                        <div class="large-6 small-12">
-                            <input
-                                class="chanel-filter"
-                                style=" margin-bottom: 10px; margin-left: 5px;"
-                                placeholder="đến"
-                                value="{{isset($_GET['price_t'])?$_GET['price_t']:''}}" type="number"
-                                id="price_t" name="price_t">
-                        </div>
-
+                <div class="large-6 small-12" style="order: 1 !important; padding-left: 10px;">
+                    <div class="large-12 small-12">
+                        <label style="margin-right:10px;">Giá: </label>
                     </div>
+                    <div class="large-6 small-12">
+                        <input
+                            class="chanel-filter"
+                            style=" margin-bottom: 10px; margin-left: 5px;"
+                            placeholder="từ"
+                            value="{{isset($_GET['price_f'])?$_GET['price_f']: ''}}" type="number"
+                            id="price_f" name="price_f">
+                    </div>
+                    <div class="large-6 small-12">
+                        <input
+                            class="chanel-filter"
+                            style=" margin-bottom: 10px; margin-left: 5px;"
+                            placeholder="đến"
+                            value="{{isset($_GET['price_t'])?$_GET['price_t']:''}}" type="number"
+                            id="price_t" name="price_t">
+                    </div>
+
                 </div>
-                <div class="large-6 small-12" style="order: 2 !important">
-                    <div class="row">
-                        <div class="large-12 small-12">
-                            <label style="margin-right:10px;">Lượng Follow: </label>
-                        </div>
-                        <div class="large-6 small-12">
-                            <input
-                                class="chanel-filter"
-                                placeholder="từ"
-                                style=" margin-bottom: 10px; margin-left: 5px;"
-                                value="{{isset($_GET['sub_f'])?$_GET['sub_f']: ''}}" type="number"
-                                id="sub_f" name="sub_f">
-                        </div>
-                        <div class="large-6 small-12">
-                            <input
-                                class="chanel-filter"
-                                placeholder="đến"
-                                style=" margin-bottom: 10px; margin-left: 5px;"
-                                value="{{isset($_GET['sub_t'])?$_GET['sub_t']: ''}}" type="number"
-                                id="sub_t" name="sub_t">
-                        </div>
+                <div class="large-6 small-12" style="order: 2 !important; padding-left: 10px;">
+                    <div class="large-12 small-12">
+                        <label style="margin-right:10px;">Lượng Follow: </label>
+                    </div>
+                    <div class="large-6 small-12">
+                        <input
+                            class="chanel-filter"
+                            placeholder="từ"
+                            style=" margin-bottom: 10px; margin-left: 5px;"
+                            value="{{isset($_GET['sub_f'])?$_GET['sub_f']: ''}}" type="number"
+                            id="sub_f" name="sub_f">
+                    </div>
+                    <div class="large-6 small-12">
+                        <input
+                            class="chanel-filter"
+                            placeholder="đến"
+                            style=" margin-bottom: 10px; margin-left: 5px;"
+                            value="{{isset($_GET['sub_t'])?$_GET['sub_t']: ''}}" type="number"
+                            id="sub_t" name="sub_t">
                     </div>
                 </div>
                 <div class="large-12" id="box-filter"

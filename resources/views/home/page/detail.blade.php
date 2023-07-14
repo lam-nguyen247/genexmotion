c0f2a543d03686fa0c0e03a741c5fd5c1c5cb5f3
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-14 09:15:30 +0700

    Update detail.blade.php

--- resources/views/home/page/detail.blade.php
+++ resources/views/home/page/detail.blade.php
@@ -13,20 +13,6 @@
                         <div class="tour-subtitle-wrapper wrapper-content">
                             {!!$page->content!!}
                         </div>
-                        <div class="tour-contact-wrapper">
-                            <img @src="/images/home/full-red-circle.png" alt="circle" class="tour-contact-circle" />
-                            <div>
-                                <a href="/@lang('contact')" class="tour-contact-direct">@lang('Contact')</a>
-                                <img @src="/images/home/arrow_forward_24px.png" alt="arrow direct" class="tour-contact-arrow" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="large-4 tour-list-item">
-                        <h4 style="text-transform: uppercase">HOME / {{$page->name}}</h4>
-                        <div class="tour-search-wrapper">
-                            <img @src="/images/home/icon-search.png" alt="icon-search" class="tour-search-icon" />
-                            <input type="text" name="search" id="search" placeholder="Search now" class="tour-search" />
-                        </div>
                     </div>
                     @include('home.includes.consultation')
                 </div>

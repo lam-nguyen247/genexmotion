037769933767df4f1bc04e79c6ca5559bada6210
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-24 14:14:48 +0700

    a

--- resources/views/home/channel/content.blade.php
+++ resources/views/home/channel/content.blade.php
@@ -60,26 +60,41 @@
         {{-- END SECTION ONE --}}
         <div style="clear: both;"></div>
         <div>
-            <table class="table-fill">
-                <thead>
-                    <tr>
-                        @foreach ($values[0] as $title)
-                            <th class="txt-left">{{$title}}</th>
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody class="table-hover">
-                    @foreach ($values as $key => $items)
-                        @if ($key > 0)
-                        <tr>
-                            @foreach ($items as $item)
-                                <td class="text-left">{{$item}}</td>
+           <div class="price">
+                <div class="price-left">
+                    <h3>Bảng giá dịch vụ tăng tương tác Facebook </h3>
+                    <ul class="price-items">
+                        <li>
+                            Tăng lượt thích (Like): Xây dựng mạng lưới người theo dõi trung thành và tăng tích cực lượt thích cho các bài đăng của bạn, giúp tăng cường sự thú vị và giá trị của nội dung.
+                        </li>
+                        <li>Tăng lượt chia sẻ (Share): Lan tỏa thông tin nhanh chóng và rộng rãi thông qua việc tăng số lượt chia sẻ bài viết, giúp tăng tầm vọng của thông điệp và thu hút thêm người tiềm năng.</li>
+                        <li>Tăng lượt bình luận (Comment): Thúc đẩy cuộc trò chuyện và tương tác tích cực qua việc tăng số lượt bình luận, giúp tạo dựng môi trường giao tiếp năng động và thú vị.</li>
+                        <li>Tăng lượt theo dõi (Follow): Xây dựng cộng đồng người hâm mộ và tăng lượng người theo dõi trên trang của bạn, giúp bạn có thêm tiềm năng tiếp cận đối tượng khách hàng rộng lớn.</li>
+                    </ul>
+                </div>
+                <div class="price-right">
+                    <table class="table-fill">
+                        <thead>
+                            <tr>
+                                @foreach ($values[0] as $title)
+                                    <th class="txt-left">{{$title}}</th>
+                                @endforeach
+                            </tr>
+                        </thead>
+                        <tbody class="table-hover">
+                            @foreach ($values as $key => $items)
+                                @if ($key > 0)
+                                <tr>
+                                    @foreach ($items as $item)
+                                        <td class="text-left">{{$item}}</td>
+                                    @endforeach
+                                </tr>
+                                @endif
                             @endforeach
-                        </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-            </table>
+                        </tbody>
+                    </table>
+                </div>
+           </div>
         </div>
         <div id="counter">
             <div class="item">

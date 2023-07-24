3ccf9f52e6a7e3e4d757a74031fc5f2e3dfe3cbb
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-24 13:42:40 +0700

    Update ChannelController.php

--- app/Http/Controllers/Home/ChannelController.php
+++ app/Http/Controllers/Home/ChannelController.php
@@ -194,7 +194,7 @@ class ChannelController extends Controller {
         $client = $this->getGoogleClient();
         $service = new Google_Service_Sheets($client);
         $spreadsheetId = '1VT8A6swg0XoKOHtEHpv07zHKIibd7SyzZ5MPB9XmAMs';
-        $range = 'Tăng tương tác Facebook!A2:G';
+        $range = 'Tăng tương tác Facebook!B2:D';
 
         // get values
         $response = $service->spreadsheets_values->get($spreadsheetId, $range);

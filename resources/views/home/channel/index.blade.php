6f92c65a3b739a76d9260a7cabc68f1b10920771
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-25 14:55:21 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -113,8 +113,8 @@
                                         {{$values[$i][2]}}
                                     </td>
                                     <td>
-                                        {{number_format($values[$i][1], 0, ',', '.')}} fls<br/>
-                                        {{number_format($values[$i][6], 0, ',', '.')}}đ<br/>
+                                        <p>{{number_format($values[$i][1], 0, ',', '.')}} fls</p>
+                                        <p> {{number_format($values[$i][6], 0, ',', '.')}}đ </p>
                                         <button onclick="go()" style="background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt mua
                                         </button>

9a4e3e3e17bed46e3d3d83e3166832d635e0c6bc
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-25 15:13:54 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -119,10 +119,6 @@
                                         alt="{{$values[$i][3]}}" target="_blank" style="background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; margin: 5px; min-width: 61px;"">Xem Group
                                         </button>
-                                        <button id="btn_link_{{$i}}" style="background-color: #8CE78C; border: 1px solid #8CE78C; margin: 5px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
-                                        0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; min-width: 61px;"
-                                                onclick="copy('link_{{$i}}')">Copy Link
-                                        </button>
                                     </td>
                                     
                                     <td>

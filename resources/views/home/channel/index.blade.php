8c548942562e02f70f11dbd650a55bba034c2f20
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-25 14:48:29 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -93,10 +93,8 @@
                             <thead>
                             <tr>
                                 <th>{{$values[0][0]}}</th>
-                                <th>{{$values[0][1]}}</th>
-                                <th>{{$values[0][6]}}</th>
-                                <th>{{$values[0][3]}}</th>
                                 <th>{{$values[0][2]}}</th>
+                                <th>Chi tiết</th>
                                 <th>{{$values[0][7]}}</th>
                             </tr>
                             </thead>
@@ -107,13 +105,14 @@
                                     <td>
                                         {{$values[$i][0]}}
                                     </td>
-                                    <td>{{number_format($values[$i][1], 0, ',', '.')}}</td>
-                                    <td>{{number_format($values[$i][6], 0, ',', '.')}} <br/>
+                                    <td>
+                                        {{$values[$i][2]}}
+                                    </td>
+                                    <td>{{number_format($values[$i][1], 0, ',', '.')}}
+                                        {{number_format($values[$i][6], 0, ',', '.')}} <br/>
                                         <button onclick="go()" style="background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt mua
                                         </button>
-                                    </td>
-                                    <td>
                                         <input value="{{$values[$i][3]}}" type="text" style="display: none;"
                                                id="link_{{$i}}"/>
                                         <button onclick="xemKenh('{{$values[$i][3]}}')" title="{{$values[$i][3]}}"
@@ -125,9 +124,7 @@
                                                 onclick="copy('link_{{$i}}')">Copy
                                         </button>
                                     </td>
-                                    <td>
-                                        {{$values[$i][2]}}
-                                    </td>
+                                    
                                     <td>
                                         {{isset($values[$i][7])?$values[$i][7]:''}}
                                     </td>

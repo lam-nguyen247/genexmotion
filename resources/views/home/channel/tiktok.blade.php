886e7c24ef70b53f0a6c38ffcac64f66459b83ac
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-28 15:24:33 +0700

    Update tiktok.blade.php

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -80,10 +80,10 @@
                                                id="link_{{$i}}"/>
                                         <button onclick="xemKenh('{{$values[$i][3]??''}}')"
                                                 title="{{$values[$i][3]??''}}" alt="{{$values[$i][3]??''}}"
-                                                target="_blank" style="color:black !important; background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
+                                                target="_blank" style="background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; margin: 5px; "">Xem Kênh
                                         </button>
-                                        <button id="btn_link_{{$i}}" style="color:black !important; background-color: #8CE78C; border: 1px solid #8CE78C; margin: 5px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
+                                        <button id="btn_link_{{$i}}" style="background-color: #8CE78C; border: 1px solid #8CE78C; margin: 5px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; "
                                                 onclick="copy('link_{{$i}}')">Copy Link
                                         </button>

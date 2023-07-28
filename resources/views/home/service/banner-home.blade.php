10b6fc56d12136996daa51d7d24d5f06148b085c
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-28 14:05:47 +0700

    a

--- resources/views/home/service/banner-home.blade.php
+++ resources/views/home/service/banner-home.blade.php
@@ -11,16 +11,13 @@
                         <img src="/images/san-xuat-video.png" alt="sản xuất video"/>
                     </div>
                     <h3 data-cms="{{app()->getLocale()}}-service-home-video-title">Sản Xuất Video</h3>
-                    <p data-cms="{{app()->getLocale()}}-service-home-video-content" style="text-align: justify;">
+                    <p data-cms="{{app()->getLocale()}}-service-home-video-content" style="text-align: justify; display: inline">
                         Tự tin là đơn vị Motion Graphic Agency với mức
                         chi phí hợp lý nhất đồng thời gia tăng chất
                         lượng truyền thông nhờ việc ứng dụng “Lean Creative Marketing” vào quy trình sản
                         xuất ý
                         tưởng và ứng dụng “One-piece Flow” trong quy trình sản xuất video
                     </p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M2 12H22M22 12L13 3M22 12L13 21" stroke="black" stroke-width="2"/>
-                    </svg>
             </div>
             </a>
         </div>
@@ -32,7 +29,7 @@
                            <img src="/images/dich-vu-marketing.png" alt="Dịch vụ Facebook"/>
                        </div>
                        <h3 data-cms="{{app()->getLocale()}}-service-home-marketing-title">Dịch Vụ Facebook</h3>
-                       <p data-cms="{{app()->getLocale()}}-service-home-marketing-content" style="text-align: justify;">
+                       <p data-cms="{{app()->getLocale()}}-service-home-marketing-content" style="text-align: justify; display: inline">
                            “Learn Creative Marketing”<br/>
                            Là một bộ công cụ siêu tinh gọn hiệu quả giúp SocMedia gia tăng hiệu quả cho
                            những ý tưởng đồng
@@ -40,10 +37,6 @@
                            thông nhanh
                            chóng, chính xác, đánh trúng tâm lý khách hàng.
                        </p>
-
-                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                           <path d="M2 12H22M22 12L13 3M22 12L13 21" stroke="black" stroke-width="2"/>
-                       </svg>
                    </div>
             </a>
         </div>
@@ -55,14 +48,11 @@
                         <img src="/images/an-pham-quang-cao.png" alt="Dịch vụ Tiktok"/>
                     </div>
                     <h3 data-cms="{{app()->getLocale()}}-service-home-ads-title">Dịch vụ Tiktok</h3>
-                    <p data-cms="{{app()->getLocale()}}-service-home-ads-content" style="text-align: justify;">
+                    <p data-cms="{{app()->getLocale()}}-service-home-ads-content" style="text-align: justify; ">
                         Với kinh nghiệm lâu năm và hiểu biết sâu sắc về cơ chế hoạt động của TikTok,
                         chúng tôi cam kết mang đến cho bạn sự tăng trưởng vượt bậc trong số lượng người theo dõi,
                         lượt xem và tương tác. Chúng tôi không chỉ tập trung vào con số, mà còn đảm bảo rằng mỗi tương tác đều là người thực sự quan tâm và tiềm năng để tạo ra sự tương tác tự nhiên và bền vững.
                     </p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M2 12H22M22 12L13 3M22 12L13 21" stroke="black" stroke-width="2"/>
-                    </svg>
                 </div>
             </a>
         </div>

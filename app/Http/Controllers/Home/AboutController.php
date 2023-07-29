890d49f78be73e5da3f11ae555567f8ee4cfcd69
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-29 12:43:34 +0700

    Update AboutController.php

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -9,7 +9,7 @@ use App\Models\Service;
 class AboutController extends Controller {
     public function index() {
         $path = 'about-us';
-        $postYoutube = Service::orderBy('id', 'desc')->paginate(6);
+        $postYoutube = Service::orderBy('id', 'desc')->paginate(8);
         return view('home.' . $path . '.index', compact('postYoutube'));
     }
 }

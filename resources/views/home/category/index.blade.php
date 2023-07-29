3e8cc3e2d00ec11e1f7eb7d8f543a0c7def4e4bd
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-29 12:39:42 +0700

    Update index.blade.php

--- resources/views/home/category/index.blade.php
+++ resources/views/home/category/index.blade.php
@@ -24,37 +24,34 @@
                                     @foreach($postYoutube as $key => $item)
                                       
                                             <div class="large-3 small-1">
-                                                <div class="row large-columns-3  medium-columns small-columns-1 has-shadow row-box-shadow-1"
-                                                    id="{{ $item->slug }}">
-                                                    <div class="col post-item">
-                                                        <div class="col-inner">
-                                                            <a href="javascript:void(0)" class="plain item-youtube" id="youtube{{ $item->id }}"
-                                                            data-id-db="{{ $item->id }}">
-                                                                <div class="box box-text-bottom box-blog-post has-hover">
-                                                                    <div class="box-image">
-                                                                        <div class="image-cover" style="padding-top:56%;">
-                                                                            <img width="300" height="135" src="{{$item->image}}"
-                                                                                class="attachment-medium size-medium wp-post-image" alt=""
-                                                                                loading="lazy"
-                                                                                sizes="(max-width: 300px) 100vw, 300px">
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="box-text text-left">
-                                                                        <div class="box-text-inner blog-post-inner">
-                                                                            <h5 class="post-title is-large ">{{ $item->name }}</h5>
-                                                                        </div>
+                                                <div class="col post-item">
+                                                    <div class="col-inner">
+                                                        <a href="javascript:void(0)" class="plain item-youtube" id="youtube{{ $item->id }}"
+                                                        data-id-db="{{ $item->id }}">
+                                                            <div class="box box-text-bottom box-blog-post has-hover">
+                                                                <div class="box-image">
+                                                                    <div class="image-cover" style="padding-top:56%;">
+                                                                        <img width="300" height="135" src="{{$item->image}}"
+                                                                            class="attachment-medium size-medium wp-post-image" alt=""
+                                                                            loading="lazy"
+                                                                            sizes="(max-width: 300px) 100vw, 300px">
                                                                     </div>
                                                                 </div>
-                                                                <div id="vidBox{{ $item->id }}" style="display: none">
-                                                                    <div id="videCont{{ $item->id }}" class="videCont">
-                                                                        <div class="yt_video">
-                                                                            <iframe id="example{{ $item->id }}" allowfullscreen="" frameborder="0"
-                                                                                    src="https://www.youtube.com/embed/{{ $item->content }}?enablejsapi=1&version=3&playerapiid=ytplayer"></iframe>
-                                                                        </div>
+                                                                <div class="box-text text-left">
+                                                                    <div class="box-text-inner blog-post-inner">
+                                                                        <h5 class="post-title is-large ">{{ $item->name }}</h5>
                                                                     </div>
                                                                 </div>
-                                                            </a>
-                                                        </div>
+                                                            </div>
+                                                            <div id="vidBox{{ $item->id }}" style="display: none">
+                                                                <div id="videCont{{ $item->id }}" class="videCont">
+                                                                    <div class="yt_video">
+                                                                        <iframe id="example{{ $item->id }}" allowfullscreen="" frameborder="0"
+                                                                                src="https://www.youtube.com/embed/{{ $item->content }}?enablejsapi=1&version=3&playerapiid=ytplayer"></iframe>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </a>
                                                     </div>
                                                 </div>
                                             </div>

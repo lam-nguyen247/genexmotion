89d27abcf781451da0b4e29bca1889e1c188f97e
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-11-09 09:16:34 +0700

    Update index.blade.php

--- resources/views/home/post/index.blade.php
+++ resources/views/home/post/index.blade.php
@@ -1,7 +1,10 @@
 @extends('home.layouts.app')
 
-@section('title', 'SocMedia Animation  - SocMedia Motion')
-@section('description', 'SocMedia Animation ĐĂNG KÝ Đăng ký Làm Video Motion Graphic Chuyên Nghiệp 00 00 00 00 Ngày Giờ Phút Giây Đăng ký ngay để nhận ưu đãi 300.000VNĐ THU HÚT VÀ ĐÁNH TRÚNG TÂM LÝKHÁCH HÀNG NHỜ VIDEO ANIMATION BỪNG SÁNG THƯƠNG HIỆU – BÙNG NỔ DOANH SỐ Nhờ chuyển động đẹp mắt')
+@section('title', 'SocMedia Animation - SocMedia Motion')
+@section('description',
+    'SocMedia Animation ĐĂNG KÝ Đăng ký Làm Video Motion Graphic Chuyên Nghiệp 00 00 00 00 Ngày Giờ
+    Phút Giây Đăng ký ngay để nhận ưu đãi 300.000VNĐ THU HÚT VÀ ĐÁNH TRÚNG TÂM LÝKHÁCH HÀNG NHỜ VIDEO ANIMATION BỪNG SÁNG
+    THƯƠNG HIỆU – BÙNG NỔ DOANH SỐ Nhờ chuyển động đẹp mắt')
 @section('css')
 @endsection
 
@@ -12,20 +15,22 @@
                 @forelse($postList as $post)
                     <div class="col post-item">
                         <div class="col-inner">
-                            <a href="{{$post->href}}" class="plain">
+                            <a href="{{ $post->href }}" class="plain">
                                 <div class="box box-text-bottom box-blog-post has-hover">
                                     <div class="box-image">
                                         <div class="image-cover" style="padding-top:56%;">
-                                            <img width="300" height="135" @src="{{$post->image}}" alt="{{$post->name}}"
-                                                 class="attachment-medium size-medium wp-post-image" loading="lazy"
-                                                 sizes="(max-width: 300px) 100vw, 300px">
+                                            <img width="300" height="135" @src="{{ $post->image }}"
+                                                alt="{{ $post->name }}"
+                                                class="attachment-medium size-medium wp-post-image" loading="lazy"
+                                                sizes="(max-width: 300px) 100vw, 300px">
                                         </div>
                                     </div>
                                     <div class="box-text text-left">
                                         <div class="box-text-inner blog-post-inner">
-                                            <h5 class="post-title is-large ">{{Str::limit($post->name, 50, $end='...')}}</h5>
+                                            <h5 class="post-title is-large ">{{ Str::limit($post->name, 50, $end = '...') }}
+                                            </h5>
                                             <div class="is-divider"></div>
-                                            <p class="from_the_blog_excerpt ">{{Str::limit($post->content, 71, $end='...(xem thêm)')}}</p>
+                                            <p class="from_the_blog_excerpt ">{!! Str::limit($post->content, 71, $end = '...(xem thêm)') !!}</p>
                                         </div>
                                     </div>
                                 </div>

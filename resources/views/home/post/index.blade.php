c5c79996d14bde33544ee525a9da3c95b10f7817
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-11-09 09:31:37 +0700

    Update index.blade.php

--- resources/views/home/post/index.blade.php
+++ resources/views/home/post/index.blade.php
@@ -29,7 +29,7 @@
                                             <h5 class="post-title is-large ">{{ Str::limit($post->name, 50, $end = '...') }}
                                             </h5>
                                             <div class="is-divider"></div>
-                                            <p class="from_the_blog_excerpt ">{!! Str::limit($post->content, 150, $end = '...(xem thêm)') !!}</p>
+                                            <p class="from_the_blog_excerpt ">{!! Str::limit($post->content, 250, $end = '...(xem thêm)') !!}</p>
                                         </div>
                                     </div>
                                 </div>

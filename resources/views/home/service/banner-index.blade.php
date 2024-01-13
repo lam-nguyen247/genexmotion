c4b3d7ad4459eff386a2e7bd17b0b0ba2276d4df
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2024-01-13 21:34:47 +0700

    Update banner-index.blade.php

--- resources/views/home/service/banner-index.blade.php
+++ resources/views/home/service/banner-index.blade.php
@@ -1,49 +1,48 @@
 <div id="content" role="main" class="content-area relative">
-    <img class="absolute socmedia_service_5" src="/images/socmedia_service_5.png" title="socmedia_service_5" alt="socmedia_service_5"/>
+    <img class="absolute socmedia_service_5" src="/images/socmedia_service_5.png" title="socmedia_service_5"
+        alt="socmedia_service_5" />
     <div class="service-wrapper">
         {{-- SECTION ONE --}}
         <div class="service-left relative">
-            <h1>{{$postServices->name}}</h1>
-            @if ($postServices->content != null && trim($postServices->content) != '')
+            <h1>{{ $postServices->name }}</h1>
+            {{-- @if ($postServices->content != null && trim($postServices->content) != '')
                 <div class="service-content">
                     {!!$postServices->content!!}
                 </div>
-            @endif
-            <img class="absolute socmedia_service_1" src="/images/socmedia_service_1.png" title="socmedia_service_1" alt="socmedia_service_1"/>
-            <img class="absolute socmedia_service_4" src="/images/socmedia_service_4.png" title="socmedia_service_4" alt="socmedia_service_4"/>
+            @endif --}}
+            <img class="absolute socmedia_service_1" src="/images/socmedia_service_1.png" title="socmedia_service_1"
+                alt="socmedia_service_1" />
+            <img class="absolute socmedia_service_4" src="/images/socmedia_service_4.png" title="socmedia_service_4"
+                alt="socmedia_service_4" />
         </div>
         <div class="service-right display-desktop">
             <div class="service-element relative">
                 <form method="post" class="service-form ">
-                    <img class="absolute socmedia_service_3" src="/images/socmedia_service_3.png" title="socmedia_service_3" alt="socmedia_service_3"/>
-                    <div  class="service-control">
+                    <img class="absolute socmedia_service_3" src="/images/socmedia_service_3.png"
+                        title="socmedia_service_3" alt="socmedia_service_3" />
+                    <div class="service-control">
                         <div class="ladi-form-item-container">
                             <div class="ladi-form-item">
                                 @csrf
-                                <input tabindex="0" name="service"
-                                       class="ladi-form-control" type="hidden"
-                                       value="{{$postServices->name}}">
-                                <input tabindex="1" name="name" required=""
-                                       id="name" required type="text"
-                                       placeholder="Họ và tên" value="">
+                                <input tabindex="0" name="service" class="ladi-form-control" type="hidden"
+                                    value="{{ $postServices->name }}">
+                                <input tabindex="1" name="name" required="" id="name" required
+                                    type="text" placeholder="Họ và tên" value="">
                             </div>
                         </div>
                     </div>
-                    <div  class="service-control">
+                    <div class="service-control">
                         <div class="ladi-form-item-container">
                             <div class="ladi-form-item">
-                                <input tabindex="3" name="phone" required="" id="phone"
-                                       required type="tel"
-                                       placeholder="Số điện thoại" value="">
+                                <input tabindex="3" name="phone" required="" id="phone" required
+                                    type="tel" placeholder="Số điện thoại" value="">
                             </div>
                         </div>
                     </div>
-                    <div  class="service-control">
+                    <div class="service-control">
                         <div class="ladi-form-item-container">
                             <div class="ladi-form-item">
-                                                    <textarea tabindex="5" name="content"
-                                                              required
-                                                              placeholder="Để lại lời nhắn cho chúng tôi"></textarea>
+                                <textarea tabindex="5" name="content" required placeholder="Để lại lời nhắn cho chúng tôi"></textarea>
                             </div>
                         </div>
                     </div>
@@ -54,22 +53,24 @@
             </div>
         </div>
 
-        <a  class="display-mobile" 
-        href="https://zalo.me/0988508769"
-        style="
+        <a class="display-mobile" href="https://zalo.me/0988508769"
+            style="
         width: 100%;
         padding: 8px 20px;
         text-align:center;
         border-radius: 20px;
         margin-bottom: 10px;
         background: linear-gradient(134deg, #EDB739 0%, #FFD061 100%);
-        ">Liên hệ ngay</a>
+        ">Liên
+            hệ ngay</a>
         {{-- END SECTION ONE --}}
 
 
         {{-- SECTION POST SERVICES --}}
     </div>
     @include('home.service.post-service')
-    <img class="absolute socmedia_service_2" src="/images/socmedia_service_2.png" title="socmedia_service_2" alt="socmedia_service_2"/>
-    <img class="absolute socmedia_service_8" src="/images/socmedia_service_8.png" title="socmedia_service_8" alt="socmedia_service_8"/>
+    <img class="absolute socmedia_service_2" src="/images/socmedia_service_2.png" title="socmedia_service_2"
+        alt="socmedia_service_2" />
+    <img class="absolute socmedia_service_8" src="/images/socmedia_service_8.png" title="socmedia_service_8"
+        alt="socmedia_service_8" />
 </div>

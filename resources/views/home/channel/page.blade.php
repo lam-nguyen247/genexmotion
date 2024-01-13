8d821514c77125474ef312466529902d69669481
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2024-01-13 21:50:38 +0700

    Update page.blade.php

--- resources/views/home/channel/page.blade.php
+++ resources/views/home/channel/page.blade.php
@@ -75,7 +75,9 @@
                                         <td>
                                             {{ $values[$i][0] }}
                                         </td>
-                                        <td>{{ number_format($values[$i][1], 0, ',', ',') }}</td>
+                                        <td>
+                                            {{ number_format($values[$i][1], 0, ',', ',') }}
+                                        </td>
                                         <td>
                                             {{ $values[$i][2] }}
                                         </td>
@@ -98,14 +100,15 @@
                                         <td>
                                             {{ $values[$i][4] }}
                                         </td>
-                                        <td>{{ number_format($values[$i][5], 0, ',', ',') }} <br />
+                                        <td>
+                                            {{ number_format($values[$i][5], 0, ',', ',') }} <br />
+                                        </td>
+                                        <td>
                                             <button onclick="go()"
                                                 style="background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt
                                                 mua
-                                            </button>
-                                        </td>
-                                        <td>
+                                            </button><br />
                                             <a href="{{ isset($values[$i][6]) ? $values[$i][6] : '' }}">
                                                 {{ isset($values[$i][6]) ? $values[$i][6] : '' }}
                                             </a>

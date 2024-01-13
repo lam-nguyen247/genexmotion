04f28588c0117657c443be9deaa6d398b5178245
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2024-01-13 21:42:30 +0700

    a

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -74,7 +74,7 @@
                                     <tr>
                                         <td>{{ $values[$i][0] ?? '' }}</td>
                                         <td>
-                                            {{ number_format($values[$i][1], 0, ',', '.') }}
+                                            {{ number_format($values[$i][1], 0, ',', ',') }}
                                         </td>
                                         <td>
                                             {{ $values[$i][2] ?? '' }}
@@ -97,7 +97,7 @@
                                         </td>
 
                                         <td>
-                                            {{ number_format($values[$i][4], 0, ',', '.') }}
+                                            {{ number_format($values[$i][4], 0, ',', ',') }}
                                         </td>
                                         <td>
                                             {{ $values[$i][5] ?? '' }}
@@ -132,7 +132,7 @@
                                             {{ $values[$i][2] ?? '' }}
                                         </td>
                                         <td>
-                                            <p>{{ number_format($values[$i][1], 0, ',', '.') }}</p>
+                                            <p>{{ number_format($values[$i][1], 0, ',', ',') }}</p>
                                             <input value="{{ $values[$i][3] ?? '' }}" type="text" style="display: none;"
                                                 id="link_{{ $i }}" />
                                             <button onclick="xemKenh('{{ $values[$i][3] ?? '' }}')"
@@ -144,7 +144,7 @@
                                             </button>
                                         </td>
                                         <td>
-                                            <p>{{ number_format($values[$i][4], 0, ',', '.') }} </p>
+                                            <p>{{ number_format($values[$i][4], 0, ',', ',') }} </p>
                                             <button onclick="go()"
                                                 style=" background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt
